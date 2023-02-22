6280c8bbf81a16d0ead708b49bbe6233278f94ea
estephant2022 <emilie.stephant2022@campus-eni.fr>
2023-02-22 16:50:36 +0100

    Mise en place de Doctrine, utilisation de requêtes simples, création de la BDD, ajout d'éléments à la BDD via un script, affichage des éléments sur les différentes pages

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,35 +2,114 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 //Attribut de la classe directement, qui permet de mutualiser le début de l'URL et du nom
 #[Route('/serie', name: 'serie_')]
 class SerieController extends AbstractController
 {
     #[Route('/list', name: 'list')]
-    public function index(): Response
+    public function index(SerieRepository $serieRepository): Response
     {
-        //TODO Récupérer la liste des séries en BDD
+        //on récupère toutes les séries en passant par le repository
+        // $series = $serieRepository->findAll();
 
-        return $this->render('serie/list.html.twig');
+        //$series = $serieRepository->findBy(["status"=>"ended"], ["popularity" => 'DESC'], 10, 10);    //Tri par popularité descendante
+        $series = $serieRepository->findBy([], ["vote" => 'DESC'], 50);    //Tri par vote, on récupère les 50 meilleurs
+
+        return $this->render('serie/list.html.twig', [
+            //on envoie tout à la vue
+            "series" => $series
+        ]);
     }
 
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])] //id doit être égal à un entier d'au moins un chiffre
-    public function show(int $id): Response
+    public function show(int $id, SerieRepository $serieRepository): Response
+    //Il aurait été possible d'écrire en paramètre : Serie $id. Ce qui veut dire que je récupère l'ensemble de l'objet à partir de son id,
+        // la récupération se fait automatiquement grâce au ParamConverter
     {
-        dump($id);
+        $serie = $serieRepository->find($id);
 
-        //TODO Récupérer des infos de la série identifiée par id
+        if (!$serie)
+            //Lance une erreur 404 si la série n'existe pas
+            throw $this->createNotFoundException("Oops ! Serie not found !");
 
-        return $this->render('serie/show.html.twig');
+        return $this->render('serie/show.html.twig',
+        ["serie"=> $serie]);
     }
 
     #[Route('/add', name: 'add')]
-    public function add(): Response
+    public function add(SerieRepository $serieRepository, EntityManagerInterface $entityManager): Response
+        //L'instance de SerieRepository est préconstruite par Symfony
+        // lorque je lui précise que j'en ai besoin d'une ... magie !
+        //Cette instance est un Singleton !
     {
+        $serie = new Serie();
+        $serie2 = new Serie();
+        $serie3 = new Serie();
+
+        $serie
+            ->setName("The Office")
+            ->setBackdrop("backdrop.png")
+            ->setDateCreated(new \DateTime())
+            ->setGenres("Comedy")
+            ->setFirstAirDate(new \DateTime('2005-03-24'))
+            ->setLastAirDate(new \DateTime('-6 month'))
+            ->setPopularity(850.52)
+            ->setPoster("poster.png")
+            ->setTmdbId(123456)
+            ->setVote(8.5)
+            ->setStatus("Ended");
+
+        $serie2
+            ->setName("Le magicien")
+            ->setBackdrop("backdrop.png")
+            ->setDateCreated(new \DateTime())
+            ->setGenres("Comedy")
+            ->setFirstAirDate(new \DateTime('2005-03-24'))
+            ->setLastAirDate(new \DateTime('-6 month'))
+            ->setPopularity(850.52)
+            ->setPoster("poster.png")
+            ->setTmdbId(123456)
+            ->setVote(8.5)
+            ->setStatus("Ended");
+
+        $serie3
+            ->setName("Le bureau des légendes")
+            ->setBackdrop("backdrop.png")
+            ->setDateCreated(new \DateTime())
+            ->setGenres("Comedy")
+            ->setFirstAirDate(new \DateTime('2005-03-24'))
+            ->setLastAirDate(new \DateTime('-6 month'))
+            ->setPopularity(850.52)
+            ->setPoster("poster.png")
+            ->setTmdbId(123456)
+            ->setVote(8.5)
+            ->setStatus("Ended");
+
+/*        dump($serie);
+
+        //Enregistrement en BDD, mise à jour automatique de l'id dans l'objet $serie
+        $serieRepository->save($serie, true);
+
+        $serie->setName("The Last of Us");          //A partir du moment où il y a un id, il comprend tout seul qu'il s'agit d'un update et non d'un create
+        $serieRepository->save($serie, true);
+
+        dump($serie);*/
+
+        $entityManager->persist($serie);
+        $entityManager->persist($serie2);
+        $entityManager->persist($serie3);
+        $entityManager->flush();
+
+        $serieRepository->remove($serie2, true);
+
         //TODO Créer un formulaire d'ajout de série
 
         return $this->render('serie/add.html.twig');
---
+++ src/Repository/SerieRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Serie>
+ *
+ * @method Serie|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Serie|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Serie[]    findAll()
+ * @method Serie[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class SerieRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Serie::class);
+    }
+
+    public function save(Serie $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity); //Préparation de la requête insert ou update, je peux en faire autant que je veux
+
+        //la différence entre insert et update est la présence d'un id dans l'objet $entity
+        if ($flush) {
+            $this->getEntityManager()->flush(); //Exécution de la requête
+        }
+    }
+
+    public function remove(Serie $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity); //Préparation de la requête delete
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Serie[] Returns an array of Serie objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('s.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Serie
+//    {
+//        return $this->createQueryBuilder('s')
+//            ->andWhere('s.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
