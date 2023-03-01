f7ab682fb40832e0d29eb644229d6ea59fa4b3ac
estephant2022 <emilie.stephant2022@campus-eni.fr>
2023-03-01 13:47:48 +0100

    Mise en place de l'authentification, des rôles, de l'entity user, màj de la BDD

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin', name: 'admin-')]
+class AdminController extends AbstractController
+
+//Juste pour tester les droits d'accès
+
+{
+    #[Route('/dashboard', name: 'dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+}
